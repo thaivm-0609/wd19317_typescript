c8224e620a3fc5ac326b9f6bf87d5b4f505f4d5b
thaivm-0609 <vuongthai95@gmail.com>
2025-01-09 18:50:03 +0700

    feat: init project

---
+++ index.js
@@ -0,0 +1,5 @@
+//cú pháp khai báo: var/let/const tenBien:kieuDuLieu = value;
+//1. Kiểu dữ liệu nguyên thủy: Number/String/Boolean:
+//1.2. Number 
+var a = 10;
+console.log(a);
---
+++ index.ts
@@ -0,0 +1,9 @@
+//cú pháp khai báo: var/let/const tenBien:kieuDuLieu = value;
+//1. Kiểu dữ liệu nguyên thủy: Number/String/Boolean:
+//1.1. Number 
+var a:number = 10;
+console.log(a);
+//1.2. String:
+var chuoi:string = 'Đây là chuỗi';
+//1.3. Boolean:
+var isValid:boolean = true;

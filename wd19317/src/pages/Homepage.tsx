4c949fb1aeadac8c01951a2557cd80e2e982518c
thaivm-0609 <vuongthai95@gmail.com>
2025-01-18 18:22:51 +0700

    feat: list + delete

--- wd19317/src/pages/Homepage.tsx
+++ wd19317/src/pages/Homepage.tsx
@@ -1,5 +1,71 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { Product } from "../types/Product"
+
 function Homepage() {
-    return <h1>Homepage</h1>
+    const [products, setProducts] = useState<Product[]>([]);
+
+    const getList = async () => {
+        try {
+            //B1: call api de lay du lieu tu json-server
+            const { data } = await axios.get('http://localhost:3000/products');
+            //B2: gọi hàm setProducts để
+            // gán dữ liệu mà json-server trả về cho biến products
+            setProducts(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const delPro = async (id: number) => {
+        try {
+            if (window.confirm('Are you sure?')) {
+                await axios.delete('http://localhost:3000/products/'+id);
+                alert('Delete success');
+                getList(); //gọi hàm getList để load lại danh sách sản phẩm mới
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getList(); //gọi hàm getList ngay khi load trang
+    }, []);
+
+    return (
+        <div>
+            <h1>Danh sách sản phẩm</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((p,index) => (
+                        <tr key={index}>
+                            <td>{p.id}</td>
+                            <td>{p.name}</td>
+                            <td>{p.price}</td>
+                            <td>
+                                <img src={p.image} alt="" />
+                            </td>
+                            <td>
+                                <a className="btn btn-info" href="">Detail</a>
+                                <a className="btn btn-warning" href="">Edit</a>
+                                <a className="btn btn-danger" onClick={() => delPro(p.id)}>Delete</a>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Homepage
